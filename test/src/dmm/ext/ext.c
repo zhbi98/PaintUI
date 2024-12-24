2f5acaae95e397bae9159a343da999a054a4dc36
zhbi98 <1523022952@qq.com>
2024-12-24 15:16:47 +0800

    显示字符串时可以给某个字段指定其他颜色，例如 Hello, #0xFF00 Red# Color

--- test/src/dmm/ext/ext.c
+++ test/src/dmm/ext/ext.c
@@ -2,6 +2,10 @@
 #include "display_string.h"
 #include "ext.h"
 
+/**
+ * When you display a string, you can specify another 
+ * Color for a field, such as Hello, # 0xFF00 Red # Color
+ */
 void textarea_string(uint32_t y, uint32_t x, uint32_t fg, uint32_t bg, 
     uint8_t font_size, uint8_t * string, uint8_t * vm)
 {
