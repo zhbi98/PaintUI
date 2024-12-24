a8a307b1b871d55fabdf6bc39ab43a626467154b
zhbi98 <1523022952@qq.com>
2024-12-24 15:07:19 +0800

    Add files via upload
    显示字符串时可以给某个字段指定其他颜色

---
+++ test/src/dmm/ext/ext.h
@@ -0,0 +1,12 @@
+/**
+ * @file ext.h
+ *
+ */
+
+#ifndef __EXT_H__
+#define __EXT_H__
+
+void textarea_string(unsigned int y, unsigned int x, unsigned int fg, unsigned int bg, 
+    unsigned char font_size, unsigned char * string, unsigned char * vm);
+
+#endif
