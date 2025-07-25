cfa6e59174f543c28e7e1c5dd34885b5d1b0ac48
zhbi98 <1523022952@qq.com>
2025-07-25 09:54:33 +0800

    更新电源管理

--- Firmware/main/src/Power/power.c
+++ Firmware/main/src/Power/power.c
@@ -7,18 +7,20 @@
  *      INCLUDES
  *********************/
 
+#include "bsp.h"
 #include "power.h"
+#include "log.h"
 
 /*********************
  *      DEFINES
  *********************/
 
-#define VOLT_MIN 2700U /*mV*/
-#define VOLT_MAX 4500U /*mV*/
+#define VOLT_MIN 2800U /*mV*/
+#define VOLT_MAX 4200U /*mV*/
 
 #define DIV 4096U /*12Bit*/
 #define VREF 280U /*2.8V, 2800mV, Magnify 100x*/
-#define RP 57U /*R(Total):R(1)=5.7(M):1(M), Magnify 10x*/
+#define RP 40U /*R(Total):R(1)=4.0(M):1(M), Magnify 10x*/
 
 #define KN 1.5f 
 
@@ -27,13 +29,21 @@
  **********************/
 
 nt_liocv_t liocv = {
-    .idx = 0,
-    .noft = 5,
-    .ret = DIV,
-    .end = 0,
+  .idx = 0,
+  .noft = 5,
+  .ret = DIV,
+  .end = 0,
 };
 
 nt_lisox_t lisox = {0};
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+static km_filte_t km_filte_volt = {.K_gain = 0.3, 
+  .last_est = VOLT_MAX, .cur_est = VOLT_MAX};
+static int16_t km_filted_volt;
 
 /**********************
  *  STATIC PROTOTYPES
@@ -41,7 +51,7 @@ nt_lisox_t lisox = {0};
 
 static void nt_bat_set_ori_val(int16_t val);
 static void nt_bat_move(int16_t data_p[], 
-    uint32_t len);
+  uint32_t len);
 
 /**********************
  *   GLOBAL FUNCTIONS
@@ -51,55 +61,55 @@ static void nt_bat_move(int16_t data_p[],
  * Emmerich Kalman estimates the current value based on historical 
  * values using an iterative method.
  */
-float kalman_filte(km_filte_t * km, float Y_meas)
+static float kalman_filte(km_filte_t * km, float Y_meas)
 {
-    km->Y = Y_meas;
-    /*Updates the current estimate*/
-    km->cur_est = km->last_est + km->K_gain * 
-        (km->Y - km->last_est);
-    /*Updates the last estimate*/
-    km->last_est = km->cur_est;
-    return km->cur_est;
+  km->Y = Y_meas;
+  /*Updates the current estimate*/
+  km->cur_est = km->last_est + km->K_gain * 
+    (km->Y - km->last_est);
+  /*Updates the last estimate*/
+  km->last_est = km->cur_est;
+  return km->cur_est;
 }
 
 /**
  * The raw open circuit voltage data of the battery 
  * is read in real time from the hardware module.
  */
-void nt_bat_set_ori_val(int16_t val)
+static void nt_bat_set_ori_val(int16_t val)
 {
-    liocv.idx = MAT_MIN(liocv.idx, DATA_NR - 1);
-    liocv.pbuffer[liocv.idx] = val;
-    liocv.idx++;
+  liocv.idx = MAT_MIN(liocv.idx, DATA_NR - 1);
+  liocv.pbuffer[liocv.idx] = val;
+  liocv.idx++;
 }
 
 /**
  * Collate data to exclude the collection of unstable data, 
  * so as not to affect the accuracy of estimates.
  */
-void nt_bat_move(int16_t data_p[], 
-    uint32_t len)
+static void nt_bat_move(int16_t data_p[], 
+  uint32_t len)
 {
-    int16_t i = 0, j = 0;
-    int16_t fval, bval;
+  int16_t i = 0, j = 0;
+  int16_t fval, bval;
 
-    int16_t * pfv, * pbv;
+  int16_t * pfv, * pbv;
 
-    for (i = 0; i < len; i++) {
-        for (j = 0; j < len - 1; 
-            j++) {
+  for (i = 0; i < len; i++) {
+    for (j = 0; j < len - 1; 
+      j++) {
 
-            pfv = &data_p[j];
-            pbv = &data_p[j + 1];
-            fval = *pfv;
-            bval = *pbv;
+      pfv = &data_p[j];
+      pbv = &data_p[j + 1];
+      fval = *pfv;
+      bval = *pbv;
 
-            if (fval > bval) {
-                *pbv = fval;
-                *pfv = bval;
-            }
-        }
+      if (fval > bval) {
+        *pbv = fval;
+        *pfv = bval;
+      }
     }
+  }
 }
 
 /**
@@ -108,69 +118,74 @@ void nt_bat_move(int16_t data_p[],
  */
 void nt_bat_ocv_est_val()
 {
-    int16_t * data_p = liocv.pbuffer;
+  int16_t * data_p = liocv.pbuffer;
 
-    if (!liocv.end) nt_bat_set_ori_val(3000);
+  km_filted_volt = kalman_filte(
+    &km_filte_volt, 
+    get_dev_adc()
+  );
 
-    if (liocv.idx >= liocv.noft) {
-        nt_bat_move(data_p, 
-            liocv.noft);
-        liocv.end = true;
-        liocv.idx = 0;
+  if (!liocv.end) nt_bat_set_ori_val(km_filted_volt);
+
+  if (liocv.idx >= liocv.noft) {
+    nt_bat_move(data_p, 
+      liocv.noft);
+    liocv.end = true;
+    liocv.idx = 0;
+  }
+
+  uint8_t valid_nr = 0;
+  uint8_t vofs = 0;
+
+  if (liocv.end) {
+    valid_nr = (int32_t)(
+      liocv.noft / 5);
+    valid_nr = MAT_MAX(valid_nr, 1);
+
+    vofs = (int32_t)(
+      (liocv.noft - 
+        valid_nr) / 2);
+
+    int32_t rt_dyn_sum = 0;
+    int32_t rt_dyn_avg = 0;
+
+    for (uint8_t i = 0; 
+      i < valid_nr; i++)
+      rt_dyn_sum += \
+        data_p[vofs + i];
+    
+    rt_dyn_avg = (int32_t)(
+      rt_dyn_sum / valid_nr);
+
+    int32_t En = 0;
+    float S2 = 0.0f;
+    int32_t dv = 0;
+    double X2 = 0.0f;
+
+    for (uint8_t j = 0; 
+      j < valid_nr; j++) {
+      dv = data_p[vofs + j] - \
+        rt_dyn_avg;
+      X2 = (int32_t)pow(dv, 2);
+      En += X2;
     }
 
-    uint8_t valid_nr = 0;
-    uint8_t vofs = 0;
+    S2 = (float)En / (float)valid_nr;
 
-    if (liocv.end) {
-        valid_nr = (int32_t)(
-            liocv.noft / 5);
-        valid_nr = MAT_MAX(valid_nr, 1);
+    liocv.end = false;
+    /*Data acquisition number dynamic 
+    transformation*/
+    liocv.noft = liocv.noft + 10;
+    liocv.noft = MAT_MIN(
+      liocv.noft, 
+      DATA_NR - 1
+    );
 
-        vofs = (int32_t)(
-            (liocv.noft - 
-                valid_nr) / 2);
-
-        int32_t rt_dyn_sum = 0;
-        int32_t rt_dyn_avg = 0;
-
-        for (uint8_t i = 0; 
-            i < valid_nr; i++)
-            rt_dyn_sum += \
-                data_p[vofs + i];
-        
-        rt_dyn_avg = (int32_t)(
-            rt_dyn_sum / valid_nr);
-
-        int32_t En = 0.0;
-        float S2 = 0.0;
-        int32_t dv = 0.0;
-        double X2 = 0.0;
-
-        for (uint8_t j = 0; 
-            j < valid_nr; j++) {
-            dv = data_p[vofs + j] - \
-                rt_dyn_avg;
-            X2 = (int32_t)pow(dv, 2);
-            En += X2;
-        }
-
-        S2 = (float)En / (float)valid_nr;
-
-        liocv.end = false;
-        /*Data acquisition number dynamic 
-        transformation*/
-        liocv.noft = liocv.noft + 10;
-        liocv.noft = MAT_MIN(
-            liocv.noft, 
-            DATA_NR - 1
-        );
-
-        /*Updates valid and stable data 
-        to the user*/
-        if (S2 <= KN) 
-            liocv.ret = rt_dyn_avg;
-    }
+    /*Updates valid and stable data 
+    to the user*/
+    if (S2 <= KN) 
+      liocv.ret = rt_dyn_avg;
+  }
 }
 
 /**
@@ -180,18 +195,41 @@ void nt_bat_ocv_est_val()
  */
 int32_t nt_bat_ocv_fit_soc()
 {
-    uint32_t volt = 0;
-    uint32_t soc = 0;
+  uint32_t volt = 0;
+  uint32_t soc = 0;
 
-    /*x = Ref-Volt * (Adc-Val / Adc-Max-Val) / (1 / RP)*/
-    volt = (int32_t)(VREF * liocv.ret * RP / DIV);
-    volt = MAT_MAX(volt, VOLT_MIN);
+  /*x = Ref-Volt * (Adc-Val / Adc-Max-Val) / (1 / RP)*/
+  volt = (int32_t)(VREF * liocv.ret * RP / DIV);
+  volt = MAT_MAX(volt, VOLT_MIN);
 
-    soc = (int32_t)((volt - VOLT_MIN) * 100.0 / 
-        (VOLT_MAX - VOLT_MIN)); /*OCV-SOC curve-fitting*/
-    soc = MAT_MIN(soc, 100);
+  soc = (int32_t)((volt - VOLT_MIN) * 100.0f / 
+    (VOLT_MAX - VOLT_MIN)); /*OCV-SOC curve-fitting*/
+  soc = MAT_MIN(soc, 100);
 
-    return soc;
+  return soc;
+}
+
+/**
+ * The curve of battery open circuit voltage and battery 
+ * capacity is fitted, and the state of 
+ * charge is estimated.
+ */
+int16_t nt_bat_ocv_fit2_soc(float volt)
+{
+  if (volt < 3.385f) volt = 3.385f;
+
+  float B1 = -119.0f * pow(volt, 2);
+  float B2 =  1026.4f * volt;
+  float B3 = -2110.3f;
+
+  /*OCV-SOC curve-fitting*/
+  int16_t Y = (int16_t)(
+    B1 + B2 + B3);
+
+  Y = MAT_MIN(Y, 100);
+  Y = MAT_MAX(Y, 10);
+
+  return Y;
 }
 
 /**
@@ -200,11 +238,18 @@ int32_t nt_bat_ocv_fit_soc()
  */
 int16_t nt_bat_get_qval()
 {
-    int16_t _fit = 0;
+  int16_t _fit = 0;
+  int16_t fit = 0;
 
-    _fit = nt_bat_ocv_fit_soc();
+  int32_t mV = (int32_t)(VREF * liocv.ret * RP / DIV);
 
-    lisox.soc = _fit;
+  fit = nt_bat_ocv_fit2_soc(mV / 1000.0f);
+  _fit = nt_bat_ocv_fit_soc();
 
-    return lisox.soc;
+  lisox.soc = _fit;
+
+  lisox.soc = (int16_t)(
+    _fit * 0.7f + fit * 0.3f);
+
+  return lisox.soc;
 }
--- Firmware/main/src/Power/power.h
+++ Firmware/main/src/Power/power.h
@@ -10,16 +10,13 @@
  *      INCLUDES
  *********************/
 
-#include <math.h>
 #include <stdbool.h>
-#include "log.h"
+#include <stdint.h>
+#include <math.h>
 
 /*********************
  *      DEFINES
  *********************/
-
-#define MAT_MIN(a, b) ((a) < (b) ? (a) : (b))
-#define MAT_MAX(a, b) ((a) > (b) ? (a) : (b))
 
 #define DATA_NR 100U
 
@@ -32,11 +29,11 @@
  * of the Kalman filter.
  */
 typedef struct {
-    int16_t pbuffer[DATA_NR];
-    uint8_t idx;
-    uint16_t noft; /**<Number of times*/
-    int16_t ret;
-    bool end;
+  int16_t pbuffer[DATA_NR];
+  uint8_t idx;
+  uint16_t noft; /**<Number of times*/
+  int16_t ret;
+  bool end;
 } nt_liocv_t;
 
 /**
@@ -44,10 +41,10 @@ typedef struct {
  * of the Kalman filter.
  */
 typedef struct {
-    float cur_est;
-    float last_est;
-    float K_gain;
-    float Y;
+  float cur_est;
+  float last_est;
+  float K_gain;
+  float Y;
 } km_filte_t;
 
 /**
@@ -55,8 +52,8 @@ typedef struct {
  * of the Kalman filter.
  */
 typedef struct {
-    uint8_t soc;
-    uint8_t soh;
+  uint8_t soc;
+  uint8_t soh;
 } nt_lisox_t;
 
 /**********************
